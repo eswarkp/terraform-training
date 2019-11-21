4b021e5b574947ad74f04e08489b84f8c35f22aa
eswaran.karuppiah <eswaran.karuppiah@eswarankaruppiahs-MacBook-Pro.local>
2019-11-21 10:51:14 +0530

    updated

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ variable "story" {
   default = "eswarkp is awesome"
 }
 
-module "ankitmodule" {
-  source = "https://github.com/AnkitKusuma566/ankit_story"
-}
+# module "ankitmodule" {
+#   source = "https://github.com/AnkitKusuma566/ankit_story"
+# }
 
 output "story" {
-  value = "${module.ankitmodule.story}  ${var.story}"
+  value = " ${var.story}"
 }
